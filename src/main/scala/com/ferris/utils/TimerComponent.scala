bc9b480f4cf7b78728b983540fc0c4c46d1e3da9
Mesfin Mebrate <mesfin.mebrate@basementcrowd.com>
2018-08-27 16:39:36 +0100

    Adding a current LocalDate function to the TimerComponent.

--- src/main/scala/com/ferris/utils/TimerComponent.scala
+++ src/main/scala/com/ferris/utils/TimerComponent.scala
@@ -1,6 +1,7 @@
 package com.ferris.utils
 
 import java.sql.Timestamp
+import java.time.LocalDate
 
 import org.mockito.Mockito._
 import org.scalatest.mockito.MockitoSugar
@@ -11,6 +12,7 @@ trait TimerComponent {
   trait Timer {
     def now: Long
     def timestampOfNow: Timestamp
+    def today: LocalDate
   }
 }
 
@@ -18,13 +20,15 @@ trait DefaultTimerComponent extends TimerComponent {
   val timer: Timer = Timer
 
   private object Timer extends Timer {
-    override def now = System.currentTimeMillis()
+    override def now: Long = System.currentTimeMillis
     override def timestampOfNow: Timestamp = new Timestamp(now)
+    override def today: LocalDate = LocalDate.now
   }
 }
 
 trait MockTimerComponent extends TimerComponent with MockitoSugar {
   override val timer: Timer = mock[Timer]
-  when(timer.timestampOfNow).thenReturn(new Timestamp(System.currentTimeMillis()))
-  when(timer.now).thenReturn(System.currentTimeMillis())
+  when(timer.timestampOfNow).thenReturn(new Timestamp(System.currentTimeMillis))
+  when(timer.now).thenReturn(System.currentTimeMillis)
+  when(timer.today).thenReturn(LocalDate.now)
 }
